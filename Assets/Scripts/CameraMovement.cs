8eb16334dbd4a4fdd2c8469d346cf5c486541913
alemkin@netchexonline.com <alemkin@netchexonline.com>
2019-04-15 13:24:22 -0400

    Added camerica script for player movement following

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public Transform Target;
+    public float Smoothing;
+
+    // Start is called before the first frame update
+    void Start () {
+        
+    }
+
+    // LateUpdate is called once per frame last
+    void LateUpdate () {
+        if (transform.position != Target.position) {
+            Vector3 targetPosition = new Vector3(Target.position.x, Target.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, Smoothing);
+        }
+    }
+}
