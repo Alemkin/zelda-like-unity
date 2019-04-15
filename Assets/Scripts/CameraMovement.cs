cd6981e52e7b4193dea6bba45b745a9ee1413039
alemkin@netchexonline.com <alemkin@netchexonline.com>
2019-04-15 14:21:52 -0400

    Added camera bounding

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -5,6 +5,8 @@ public class CameraMovement : MonoBehaviour
 
     public Transform Target;
     public float Smoothing;
+    public Vector2 MaxPosition;
+    public Vector2 MinPosition;
 
     // Start is called before the first frame update
     void Start () {
@@ -15,6 +17,8 @@ public class CameraMovement : MonoBehaviour
     void LateUpdate () {
         if (transform.position != Target.position) {
             Vector3 targetPosition = new Vector3(Target.position.x, Target.position.y, transform.position.z);
+            targetPosition.x = Mathf.Clamp(targetPosition.x, MinPosition.x, MaxPosition.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, MinPosition.y, MaxPosition.y);
             transform.position = Vector3.Lerp(transform.position, targetPosition, Smoothing);
         }
     }
