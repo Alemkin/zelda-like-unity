eff906463e536a4940bfb002311043270fc9ed9a
alemkin@netchexonline.com <alemkin@netchexonline.com>
2019-04-15 09:59:41 -0400

    Initial commit working on zelda clone

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed;
+    private Rigidbody2D _playerRigidBody;
+    private Vector3 _change;
+    private Animator _animator;
+    
+    // Start is called before the first frame update
+    void Start () {
+        _playerRigidBody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        _change = Vector3.zero;
+        _change.x = Input.GetAxisRaw("Horizontal");
+        _change.y = Input.GetAxisRaw("Vertical");
+        UpdateAnimationAndMove();
+    }
+
+
+    void UpdateAnimationAndMove () {
+        if (_change != Vector3.zero)
+        {
+            MoveCharacter();
+            _animator.SetFloat("moveX", _change.x);
+            _animator.SetFloat("moveY", _change.y);
+            _animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            _animator.SetBool("isMoving", false);
+        }
+    }
+
+    void MoveCharacter () {
+        Vector2 newPosition = transform.position + (_change * Speed * Time.deltaTime);
+        _playerRigidBody.MovePosition(newPosition);
+    }
+}
