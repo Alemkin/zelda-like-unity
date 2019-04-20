848c0284f54b4be4e4603daea64971bf7acf16b8
alemkin@netchexonline.com <alemkin@netchexonline.com>
2019-04-20 15:51:12 -0400

    Added new lake tiled map, and room transitions

---
+++ Assets/Scripts/RoomMove.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class RoomMove : MonoBehaviour
+{
+    public Vector2 CameraChange;
+    public Vector3 PlayerChange;
+    private CameraMovement _camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = Camera.main.GetComponent<CameraMovement>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.CompareTag("Player"))
+        {
+            _camera.MinPosition += CameraChange;
+            _camera.MaxPosition += CameraChange;
+            collider.transform.position += PlayerChange;
+        }
+    }
+
+}
